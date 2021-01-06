e8cb718db20a07ef0ad460fb73bfcb3ebe5a8993
Nadilson <nadilson@protonmail.com>
2021-01-06 07:46:05 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -85,6 +85,14 @@ public class Order implements Serializable {
 	public void setStatus(OrderStatus status) {
 		this.status = status;
 	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
 
 	@Override
 	public int hashCode() {
