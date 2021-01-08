8ca4fc11bdbc10c0f308833fe5d2e521fe8cddfe
Nadilson <nadilson@protonmail.com>
2021-01-08 05:17:01 -0300

    List products and prices from back-end

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,26 @@
 import './styles.css';
-import { ReactComponent as Logo } from './logo.svg';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from './../api';
+
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+       // console.log('Componente Orders iniciou!')
+       fetchProducts().then(response => 
+          setProducts(response.data))
+          .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
