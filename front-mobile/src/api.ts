66da8cdf53a5b67fece5baa3cefb54d74b6d1e00
Nadilson <nadilson@protonmail.com>
2021-01-11 01:45:16 -0300

    Add OrderDetails (APP FINISHED!)

---
+++ front-mobile/src/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = 'https://nadilson-sds2.herokuapp.com';
+//const API_URL = 'http://192.168.1.110:8080';
+
+export function fetchOrders() {
+   return axios(`${API_URL}/orders`)
+}
+
+export function confirmDelivery(orderId: number) {
+   return axios.put(`${API_URL}/orders/${orderId}/delivered`)
+}
